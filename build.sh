3ad6b25052545f5598ba7bffe4b8e60a3ffb981c
markfirmware <markfirmware@users.noreply.github.com>
2016-11-19 04:03:31 -0500

    appveyor

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ fi
 
 date
 
-#appveyor AddMessage "compiling ultibo-retromachine"
-#/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/fpc \
-# -B \
-# -Tultibo \
-# -Parm \
-# -CpARMV7A \
-# -WpRPI3B \
-# @/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/RPI3.CFG \
-# -O2 \
-# Project1.pas
+appveyor AddMessage "compiling ultibo-retromachine"
+/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/fpc \
+ -B \
+ -Tultibo \
+ -Parm \
+ -CpARMV7A \
+ -WpRPI3B \
+ @/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/RPI3.CFG \
+ -O2 \
+ Project1.pas
 
 date
